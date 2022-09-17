f6d9f16a30ae1d097d8d4cc044ed0ff8a84f8d50
Grayson Allder <graysonallder@Graysons-MacBook-Pro.local>
2022-09-16 23:25:01 -0700

    A very bad version of hello world.

---
+++ main.py
@@ -0,0 +1,30 @@
+# a horribly inefficient program :P
+import codecs
+def p89r(skk):
+	print("\033[96m {}\033[00m".format(skk), end="")
+CVX5 = (('xnkghyjkolsr'))
+x = 0
+def p101r(Qark567,Y85,y):
+	Qark567 = Delta9
+	Y85 = Y85
+	for _ in Delta9:
+		if y <= Y85:
+			if y % 2 == 0:
+				_ = (codecs.decode(_, "hex").decode('utf-8'))
+				p99r(_)
+				y += 1
+			else:
+				_ = (codecs.decode(_, "hex").decode('utf-8'))
+				p89r(_)
+				y += 1
+def p99r(skk):
+	print("\033[95m {}\033[00m".format(skk), end="")
+class Zql45hy_jlsdhlhs678sdokuasbsdl :
+	X28b = ('48:65:6c:6c:6f:20:57:6f:72:6c:64:21')
+	B7_5bjusboa_nb76_8ghN = X28b
+	skda_Ghjs8687_VVskuEWKNSLA = B7_5bjusboa_nb76_8ghN
+Djklu783 = Zql45hy_jlsdhlhs678sdokuasbsdl()
+C_develop_13R = Djklu783.skda_Ghjs8687_VVskuEWKNSLA
+Delta9 = XA28 = C_develop_13R.split(':')
+Zeta = int(len([ele for ele in CVX5 if ele.isalpha()]))
+p101r(Delta9,Zeta,x)
